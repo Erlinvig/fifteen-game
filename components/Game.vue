fb6b861e80490c12e32eec9e6803d757477db0f3
erlinvig <erlinvig@gmail.com>
2019-10-10 21:44:00 +0300

    fix(ui): remove vuex

--- components/Game.vue
+++ components/Game.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
+    <div class="mt1">
+      <transition
+        name="fade"
+      >
+        <div
+          v-if="isVictory"
+          class="notification"
+        >
+          Победная комбинация
+        </div>
+      </transition>
+    </div>
+
+    <div class="panel">
+      <div class="panel__amount"> Шаги: {{amount}}</div>
+      <div class="panel__size">
+        <button @click="decrease" class="panel__size__decrease"> - </button>
+        <div class="panel__size__value">{{getLineSize}}</div>
+        <button @click="increase" class="panel__size__increase"> + </button>
+      </div>
+      <button
+        class="panel__shuffle"
+        @click="shuffle"
+      >
+        Перемешать
+      </button>
+    </div>
+
     <transition-group name="list-complete" tag="div" class="game-field">
       <span
         v-for="(item, index) in getItems"
@@ -29,31 +57,236 @@
 <script>
   export default {
     mounted() {
-      this.$store.dispatch('game/init');
+      this.amountStep = 0;
+      this.items = [];
+
+      for (let i = 1; i < this.lineSize * this.lineSize; i++) {
+        this.items.push( {value: i} )
+      }
+      this.items.push( {value: 0} );
     },
     data() {
       return {
         items: [],
-        nextNum: 10
+        lineSize: 4,
+        amountStep: 0,
+        isVictory: true,
       }
     },
     methods: {
       step(index) {
-        this.$store.dispatch('game/step', {index})
+        let lineSize = Math.sqrt(this.items.length);
+
+        let isStart = index === 0;
+        let isEnd = index === this.items.length - 1;
+        let isUpLine = index < lineSize;
+        let isDownLine = index > this.items.length - lineSize - 1;
+
+        let isRightLine = index % lineSize === 0;
+        let isLeftLine = index % lineSize === lineSize-1;
+
+        if (!isEnd && !isLeftLine) {
+          if (this.items[index + 1].value === 0) {
+            this.items[index + 1].value = this.items[index].value;
+            this.items[index].value = 0;
+
+            this.amountStep++;
+          }
+        }
+
+        if (!isStart && !isRightLine) {
+          if (this.items[index - 1].value === 0) {
+            this.items[index - 1].value = this.items[index].value;
+            this.items[index].value = 0;
+
+            this.amountStep++;
+          }
+        }
+
+        if (!isUpLine) {
+          if (this.items[index - lineSize].value === 0) {
+            this.items[index - lineSize].value = this.items[index].value;
+            this.items[index].value = 0;
+
+            this.amountStep++;
+          }
+        }
+
+        if (!isDownLine) {
+          if (this.items[index + lineSize].value === 0) {
+            this.items[index + lineSize].value = this.items[index].value;
+            this.items[index].value = 0;
+
+            this.amountStep++;
+          }
+        }
+        this.checkOnVictory()
+      },
+
+      checkOnVictory() {
+        for (let i = 0; i < this.items.length-1; i++) {
+          if (this.items[i].value === i+1) {
+            this.isVictory = true;
+          } else {
+            this.isVictory = false;
+            break;
+          }
+        }
+      },
+
+      shuffle() {
+        this.amountStep = 0;
+
+        let j, temp;
+        for(let i = this.items.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random()*(i + 1));
+          temp = this.items[j].value;
+          this.items[j].value = this.items[i].value;
+          this.items[i].value = temp;
+        }
+        this.checkOnVictory()
+      },
+      reinit() {
+        this.items = [];
+        for (let i = 1; i < this.lineSize * this.lineSize; i++) {
+          this.items.push( {value: i} )
+        }
+        this.items.push({value: 0});
+      },
+      decrease() {
+        if (this.lineSize > 2) {
+          this.lineSize--;
+        }
+        this.reinit()
+      },
+      increase() {
+        if (this.lineSize < 7) {
+          this.lineSize++;
+        }
+        this.reinit()
       }
     },
     computed: {
       getItems() {
-        return this.$store.getters['game/getItems']
+        return this.items
       },
       getLineSize() {
-        return this.$store.getters['game/getLineSize']
+        return this.lineSize
+      },
+      amount() {
+        return this.amountStep
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  .mt1 {
+    margin-top: 1em;
+  }
+  .notification {
+    width: 100%;
+    height: 50px;
+    background-color: #d2ffd6;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 18px;
+    border: 1px solid #90c9a0;
+  }
+
+  .fade-enter-active {
+    animation: fade .5s;
+  }
+  .fade-leave-active {
+    animation: fade .5s reverse;
+  }
+  @keyframes fade {
+    0% {
+      max-height: 0;
+      transform: scale(0);
+      opacity: 0;
+    }
+    100% {
+      max-height: 50px;
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+
+  .panel {
+    flex-wrap: wrap;
+    display: flex;
+    margin: 1em 0;
+    padding: 1em;
+    border: 1px solid #c3c3c3;
+    background-color: #f1f1f1;
+    justify-content: space-between;
+
+    &__amount {
+      font-size: 22px;
+      background-color: #d7d7d7;
+      border: 1px solid #c3c3c3;
+      padding: 1em;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    &__size {
+      display: flex;
+      background-color: #d7d7d7;
+      border: 1px solid #c3c3c3;
+      align-items: center;
+      padding: 1em;
+      justify-content: center;
+
+      &__decrease, &__increase {
+        font-size: 20px;
+        display: flex;
+        background-color: #fff;
+        padding: .5em;
+        outline: none;
+        border: none;
+        cursor: pointer;
+        transition: .35s;
+        &:hover {
+          background-color: #cfffc9;
+        }
+      }
+      &__increase {
+        border-top-right-radius: 50%;
+        border-bottom-right-radius: 50%;
+      }
+      &__decrease {
+        border-top-left-radius: 50%;
+        border-bottom-left-radius: 50%;
+      }
+      &__value {
+        font-size: 24px;
+        display: flex;
+        background-color: #d7ffd9;
+        border: 1px solid #3b8070;
+        padding: .5em;
+      }
+    }
+
+    &__shuffle {
+      font-size: 18px;
+      min-width: 10em;
+      outline: none;
+      background-color: #d7d7d7;
+      border: 1px solid #c3c3c3;
+      border-radius: .5em;
+      padding: 1em;
+      cursor: pointer;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
   button {
     outline: none;
     border: none;
@@ -90,5 +323,18 @@
     width: 25%;
     height: 25%;
     display: inline-block;
+  }
+
+  @media (max-width: 767px) {
+    .panel {
+      &__shuffle {
+        margin-top: 1em;
+        width: 100%;
+      }
+
+      &__amount, &__size {
+        width: 47.5%;
+      }
+    }
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="container">
-    <victory-notification></victory-notification>
-    <panel></panel>
     <game></game>
   </div>
 </template>
 
 <script>
-  import VictoryNotification from '~/components/VictoryNotification.vue'
-  import Panel from '~/components/panel.vue'
   import Game from '~/components/Game.vue'
 
   export default {
     components: {
-      VictoryNotification,
-      Panel,
       Game
     }
   }
